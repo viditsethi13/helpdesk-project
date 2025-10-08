5f4612d560bc5f1ca66ca5496360f4a7df1a19b4
vidit sethi <viditsethi13@gmail.com>
2025-10-08 17:42:37 -0500

    App added to web via expo

---
+++ helpdesk-app/app/index.js
@@ -0,0 +1,7 @@
+// app/index.js
+
+// This imports your App.js component from the parent directory
+import App from '../App';
+
+// This exports it as the default component for the web's root route (/)
+export default App;
